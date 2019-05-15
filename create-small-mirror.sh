be86d98464e316f121c1af04df936ba6fcabd88b
Dharhas Pothina <dharhas@quansight.com>
2019-05-15 13:41:42 -0500

    add small mirror test to pipeline

---
+++ create-small-mirror.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# activate conda
+eval "$(conda shell.bash hook)"
+
+# create local mirror
+conda create -y -n mirror-env -c conda-forge conda-mirror pytest
+conda activate mirror-env
+
+echo 'creating local mirror of quansight-small-test channel ...'
+conda mirror -vv --upstream-channel quansight-small-test --target-directory mirror --platform linux-64
+conda mirror -vv --upstream-channel quansight-small-test --target-directory mirror --platform noarch
+conda deactivate mirror-env
+
+# create web and offline mirror
+echo 'creating new envs ...'
+conda create -y -n small-from-web -c quansight-small-test --override-channels python=3.7 conda
+conda create -y -n small-from-mirror -c ./mirror --override-channels python=3.7 conda
+conda create -y --offline -n small-from-mirror-offline -c ./mirror --override-channels python=3.7 conda
+
+# create conda env package lists as json 
+echo 'saving env package lists as json ...'
+conda list small-from-web --json > small-from-web.json
+conda list small-from-mirror --json > small-from-mirror.json
+conda list small-from-mirror-offline --json > small-from-mirror-offline.json
+
+echo '-------------------------------'
+cat small-from-web.json
+echo '-------------------------------'
+cat small-from-mirror.json
+echo '-------------------------------'
+cat small-from-mirror-offline.json
