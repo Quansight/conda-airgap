fdfba375a90a1f9670eceaf2517b169dd82ebcd6
Anthony Scopatz <scopatz@gmail.com>
2019-05-31 17:04:09 -0400

    fake proxy tests

--- test_full_mirror.py
+++ test_full_mirror.py
@@ -9,6 +9,14 @@ import pytest
 
 
 CHANNEL = "quansight-small-test"
+PROXY_SETTINGS = {
+    "HTTP_PROXY": "http://fake.proxy.server/",
+    "HTTPS_PROXY": "https://fake.proxy.server/",
+    "NO_PROXY": "localhost,127.0.0.1,0.0.0.0",
+    "http_proxy": "http://fake.proxy.server/",
+    "https_proxy": "https://fake.proxy.server/",
+    "no_proxy": "localhost,127.0.0.1,0.0.0.0",
+}
 
 
 @pytest.fixture(scope="module")
@@ -62,11 +70,28 @@ def test_env_creation_http(setup_mirror_server, pkg_list):
 
 
 @pytest.mark.parametrize("pkg_list", [["python"], ["python", "conda"]])
-def test_env_creation_condarc(setup_mirror_server, pkg_list):
+def test_env_creation_http_no_proxy(setup_mirror_server, pkg_list):
+    # add some bad proxy setting to make sure that we are really getting local
+    # packages only.
     env_name = "_".join(pkg_list)
     env = dict(os.environ)
-    env["CONDARC"] = "./condarc"
+    env.update(PROXY_SETTINGS)
 
+    subprocess.run(
+        ["./create-env.sh", f"http://localhost:8000/{CHANNEL}", env_name, *pkg_list],
+        env=env
+    )
+
+    fweb = f"test-data/{env_name}-from-web.json"
+    fmirror = f"test-data/{env_name}-from-mirror.json"
+    foffline = f"test-data/{env_name}-from-mirror-offline.json"
+
+    assert _clean_json(fweb) == _clean_json(fmirror)
+    assert _clean_json(fmirror) == _clean_json(foffline)
+
+
+def check_only_offline(env, pkg_list):
+    env_name = "_".join(pkg_list)
     print("-----------------------------------------------------------------")
     print(f"Creating new envs from web and offline mirrors using packages: {pkg_list}")
     print("-----------------------------------------------------------------")
@@ -90,6 +115,21 @@ def test_env_creation_condarc(setup_mirror_server, pkg_list):
     data = _clean_json(foffline)
     pkg_names = {rec["name"] for rec in data}
     assert set(pkg_list) <= pkg_names
+
+
+@pytest.mark.parametrize("pkg_list", [["python"], ["python", "conda"]])
+def test_env_creation_condarc(setup_mirror_server, pkg_list):
+    env = dict(os.environ)
+    env["CONDARC"] = "./condarc"
+    check_only_offline(env, pkg_list)
+
+
+@pytest.mark.parametrize("pkg_list", [["python"], ["python", "conda"]])
+def test_env_creation_condarc_no_proxy(setup_mirror_server, pkg_list):
+    env = dict(os.environ)
+    env["CONDARC"] = "./condarc"
+    env.update(PROXY_SETTINGS)
+    check_only_offline(env, pkg_list)
 
 
 def _clean_json(fname):
