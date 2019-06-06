2db224ad85536fb1c8624d21021e3e5cd5a20c81
Anthony Scopatz <scopatz@gmail.com>
2019-06-06 11:42:32 -0400

    install from quansight

--- create-mirror.sh
+++ create-mirror.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 # Creates local mirror of a conda channel
 
-display_usage() { 
+display_usage() {
     echo "Creates local mirror of a conda channel"
     echo
     echo "Usage: create-mirror.sh <channel> [flags]"
     echo "  channel: channel to be mirrored"
     echo "  flags: flags to pass to conda mirror"
-	} 
+	}
 
-if [[ $# == 0 || $1 == "--help" ||  $1 == "-h" ]] 
-then 
+if [[ $# == 0 || $1 == "--help" ||  $1 == "-h" ]]
+then
     display_usage
     exit 0
 fi
@@ -20,7 +20,7 @@ eval "$(conda shell.bash hook)"
 
 # create local mirror
 echo "creating conda env with conda-mirror installed"
-conda create -y -n mirror-env -c conda-forge conda-mirror
+conda create -y -n mirror-env -c conda-forge -c quansight conda-mirror
 conda activate mirror-env
 
 echo "creating local mirror of $1 channel (linux-64, noarch)..."
